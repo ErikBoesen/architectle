47732ebc5b1b4d586e72d46118aad15f37403e77
ErikBoesen <me@erikboesen.com>
2025-01-22 02:11:57 -0500

    Clean up logic for game flow

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -22,22 +22,27 @@ function App() {
 
   const handleGuess = () => {
     const correctYear = buildings[round].year;
-    const difference = Math.abs(correctYear - userGuess) - 10;
-    setLives(prevScore => Math.max(prevScore - difference, 0)); // Ensure score doesn't go below 0
+    const difference = Math.abs(correctYear - userGuess);
+
+    if (difference > 10) {
+        setLives(prevScore => Math.max(prevScore - difference, 0)); // Subtract difference if greater than 10
+    } else {
+        setLives(prevScore => prevScore + (10 - difference), 10); // Add lives back for being within 10 years
+    }
+
     setRound(prevIndex => (prevIndex + 1) % buildings.length); // Move to next building
     setUserGuess(0); // Reset user guess
 
     // Set last guess details
     setLastGuess({
-      buildingName: buildings[round].name,
-      guessedYear: userGuess,
-      difference: difference,
-      correct: correctYear === userGuess,
+        building: buildings[round],
+        guessedYear: userGuess,
+        correct: correctYear === userGuess,
     });
 
     // Dismiss last guess message after 5 seconds
     setTimeout(() => {
-      setLastGuess(null);
+        setLastGuess(null);
     }, 5000);
   };
 
@@ -48,7 +53,10 @@ function App() {
           <img src='logo512.png' alt='Logo' />
           <h1>Architectle</h1>
         </div>
-        <p>❤️{lives}</p>
+        <div id='scoring'>
+          <p>{lives} ❤️</p>
+          <p>{round} 🏆</p>
+        </div>
       </header>
       <main>
         {buildings.length > 0 && (
@@ -70,16 +78,14 @@ function App() {
             {lastGuess && (
               <div>
                 <p>
-
-                </p>
-                <p>
-                   <a href={'https://en.wikipedia.org/wiki/' + lastGuess.buildingName.replace(' ', '_')}>{lastGuess.buildingName}</a> was built in  {lastGuess.guessedYear}. {lastGuess.correct ? 'Good guess!' : `-${lastGuess.difference} lives!`}
+                   <a href={'https://en.wikipedia.org/wiki/' + lastGuess.building.name.replace(' ', '_')}>{lastGuess.building.name}</a> was finished in {lastGuess.building.year}, you guessed {lastGuess.guessedYear}. {lastGuess.correct ? 'Perfect guess!' : `-${lastGuess.difference} lives!`}
                 </p>
               </div>
             )}
           </div>
         )}
       </main>
+      <footer>A game by <a href="https://erikboesen.com">Erik Boesen</a>.</footer>
     </div>
   );
 }
